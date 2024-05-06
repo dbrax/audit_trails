6d596e28888f900016ec3aebecca32b274fbc826
Emmanuel <epmnzava@gmail.com>
2024-05-06 12:07:32 +0300

    updated code

--- src/AuditTrails.php
+++ src/AuditTrails.php
@@ -13,7 +13,24 @@ class AuditTrails
     {
     }
 
-    public function logdata(string $event, string $event_category, array $request, array $response, string $ip, string $fullUrl, string $device, string $comment, $userid = null, $createdby = null, $request_id = null, $session_id = null)
+    public function store_log(string $event, string $event_category, array $request, array $response, string $ip, string $fullUrl, string $device, string $comment, $userid = null, $createdby = null,  $platform = null)
+    {
+        $audit = new AuditTrail;
+        $audit->event = $event;
+        $audit->event_category = $event_category;
+        $audit->request = json_encode($request);
+        $audit->response = json_encode($response);
+        $audit->ip = $ip;
+        $audit->comment = $comment;
+        $audit->userid = $userid;
+        $audit->fullUrl = $fullUrl;
+        $audit->device = $device;
+        $audit->plaftorm = $platform;
+        $audit->created_by = $createdby;
+        $audit->save();
+    }
+
+    public function logdata(string $event, string $event_category, array $request, array $response, string $ip, string $fullUrl, string $device, string $comment, $userid = null, $createdby = null,  $request_id = null, $session_id = null)
     {
         $audit = new AuditTrail;
         $audit->event = $event;
